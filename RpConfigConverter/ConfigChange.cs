33c0f633e27bceb3313dd3a8d5cce20ccf29e452
dmva <dmva@ua.fm>
2021-04-09 16:32:07 +0300

    init

---
+++ RpConfigConverter/ConfigChange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpConfigConverter
+{
+    public class ConfigChange
+    {
+        public List<Variable> Variables { get; set; }
+        public List<Change> Changes { get; set; }
+    }
+}
---
+++ RpConfigConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpConfigConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var changes = CreateChanges();
+        }
+
+        private static ConfigChange CreateChanges()
+        {
+            var change = new ConfigChange();
+            change.Variables = new List<Variable>();
+            change.Variables.Add(new Variable() { Name = "Machine", Value = "DK03SV001547" });
+            change.Changes.Add(new Change() { Value = "http://Machine/C1Api/", Attribute = "Value", XPath = "/configuration/appSettings/add[@key='api.Address']" });
+            return change;
+        }
+    }
+}
