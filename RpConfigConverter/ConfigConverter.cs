c140ac770378db586cd998f0180a8c04d34005c7
dmva <dmva@ua.fm>
2021-04-09 17:31:32 +0300

    init

---
+++ RpConfigConverter/ConfigConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace RpConfigConverter
+{
+    public class ConfigConverter
+    {
+        public void Convert(string configFile, ConfigChange changeDefinition)
+        {
+         
+                if (!File.Exists(configFile))
+                    throw new FileNotFoundException($"file {configFile} not found");
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.PreserveWhitespace = true;
+                xmlDoc.Load(configFile);
+
+                foreach (var configChange in changeDefinition.Changes)
+                {
+                    ApplyConfigChange(xmlDoc, configChange, changeDefinition.Variables);
+                }
+
+                xmlDoc.Save(configFile);
+            
+        }
+        private void ApplyConfigChange(XmlDocument configDoc, Change configChange, List<Variable> variables)
+        {
+            XmlNode node = configDoc.SelectSingleNode(configChange.XPath);
+            if (node == null)
+            {
+                throw new ApplicationException($"node {configChange.XPath} not found");
+            }
+
+            if (!string.IsNullOrEmpty(configChange.Attribute))
+            {
+                if (node.Attributes == null)
+                {
+                    throw new ApplicationException(
+                        $"node {configChange.XPath} does not have attributes");
+                }
+                XmlAttribute attributeToChange = node.Attributes[configChange.Attribute];
+                if (attributeToChange == null)
+                {
+                    throw new ApplicationException(
+                        $"node {configChange.XPath} does not have attribute {configChange.Attribute}");
+                }
+                attributeToChange.Value = GetConfigChangeValue(configChange.Value, variables);
+            }
+        }
+
+        private string GetConfigChangeValue(string value, List<Variable> variables)
+        {
+            string newValue = value;
+            foreach(var replacement in variables)
+            {
+                newValue = newValue.Replace(replacement.Name, replacement.Value);
+            }
+
+            return newValue;
+        }
+    }
+}
--- RpConfigConverter/Program.cs
+++ RpConfigConverter/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace RpConfigConverter
 {
@@ -11,14 +13,80 @@ namespace RpConfigConverter
         static void Main(string[] args)
         {
             var changes = CreateChanges();
+            SaveChanges(changes);
+            LoadChanges(args[1]);
+            List<Variable> variables = LoadVariables(args);
+            if (changes.Variables == null)
+                changes.Variables = new List<Variable>();
+            if (changes.Changes == null)
+                changes.Changes = new List<Change>();
+            changes.Variables = MergeVariables(changes.Variables, variables);
+            var converter = new ConfigConverter();
+            converter.Convert(args[0], changes);
+        }
+
+        private static List<Variable> MergeVariables(List<Variable> initial, List<Variable> replace)
+        {
+            var result = new List<Variable>();
+            foreach(var v in initial)
+            {
+                var merged = new Variable() { Name = v.Name, Value = v.Value };
+                var replaceVal = replace.FirstOrDefault(x => x.Name == v.Name);
+                if (replaceVal != null)
+                    merged.Value = replaceVal.Value;
+                result.Add(merged);
+            }
+
+            foreach (var v in replace)
+            {
+                var initVal = initial.FirstOrDefault(x => x.Name == v.Name);
+                if (initVal == null)
+                {
+                    var merged = new Variable() { Name = v.Name, Value = v.Value };
+                    result.Add(merged);
+                }
+            }
+            return result;
+        }
+
+        private static List<Variable> LoadVariables(string[] args)
+        {
+            var result = new List<Variable>();
+            if (args.Length > 2)
+            {
+                for(int i=2;i<args.Length;i++)
+                {
+                    var pair = args[i];
+                    var pairParts = pair.Split(':');
+                    var variable = new Variable() { Name = pairParts[0], Value = pairParts[1] };
+                    result.Add(variable);
+                }
+            }
+            return result;
+        }
+
+        private static ConfigChange LoadChanges(string fileName)
+        {
+            string jsonStr = File.ReadAllText(fileName);
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            ConfigChange script = js.Deserialize<ConfigChange>(jsonStr);
+            return script;
+        }
+
+        private static void SaveChanges(ConfigChange script)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            var jsonString = js.Serialize(script);
+            File.WriteAllText("configchange.json", jsonString);
         }
 
         private static ConfigChange CreateChanges()
         {
             var change = new ConfigChange();
             change.Variables = new List<Variable>();
-            change.Variables.Add(new Variable() { Name = "Machine", Value = "DK03SV001547" });
-            change.Changes.Add(new Change() { Value = "http://Machine/C1Api/", Attribute = "Value", XPath = "/configuration/appSettings/add[@key='api.Address']" });
+            change.Changes = new List<Change>();
+            change.Variables.Add(new Variable() { Name = "machine", Value = "DK03SV001547" });
+            change.Changes.Add(new Change() { Value = "http://machine/C1Api/", Attribute = "value", XPath = "/configuration/appSettings/add[@key='api.Address']" });
             return change;
         }
     }
